378bd16eb99f1aaf9c2281d321b8a61914393a8e
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-10-13 00:36:59 -0300

    Update shell script

--- Source code/RunScripts/run_map_2.sh
+++ Source code/RunScripts/run_map_2.sh
@@ -1,3 +1,3 @@
-gcc -o game ../main_code/main.c ../libs_source_code/tArquivos.c ../libs_source_code/tPosicoes.c ../libs_source_code/tEstatisticas.c ../libs_source_code/tMainSnake.c
+gcc ../main_code/main.c ../libs_source_code/tArquivos.c ../libs_source_code/tPosicoes.c ../libs_source_code/tEstatisticas.c ../libs_source_code/tMainSnake.c -o game 
 ./game ../../Maps/2
 rm game
